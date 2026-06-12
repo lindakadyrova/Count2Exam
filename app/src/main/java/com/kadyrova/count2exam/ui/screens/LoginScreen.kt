f72983f6828e1016a031d0ba3c88c5659c40d64a
Linda Kadyrova <linda.kadyrova@edu.fh-joanneum.at>
2026-06-13 01:52:23 +0200

    Add email field

--- app/src/main/java/com/kadyrova/count2exam/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/kadyrova/count2exam/ui/screens/LoginScreen.kt
@@ -1,33 +1,21 @@
 package com.kadyrova.count2exam.ui.screens
 
-import android.R.attr.password
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicSecureTextField
-import androidx.compose.foundation.text.input.TextFieldState
-import androidx.compose.foundation.text.input.TextObfuscationMode
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.material.icons.Icons
@@ -51,13 +39,10 @@ fun FirstScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            state = rememberTextFieldState(),
-            label = { Text("Benutzername") }
-        )
+        EmailTextField()
+
         Spacer(modifier = Modifier.height(16.dp))
-//
+
         PasswordTextField("Passwort")
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -71,7 +56,7 @@ fun FirstScreen() {
             modifier = Modifier.fillMaxWidth(),
             onClick = { }) {
 
-            Text("Register")
+            Text("Registrieren")
         }
     }
 }
--- app/src/main/java/com/kadyrova/count2exam/ui/screens/PWForgottenScreen.kt
+++ app/src/main/java/com/kadyrova/count2exam/ui/screens/PWForgottenScreen.kt
@@ -8,17 +8,29 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.kadyrova.count2exam.ui.screens.PasswordTextField
 
 @Composable
-fun RegisterScreen() {
+fun PWForgottenScreen() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -28,50 +40,35 @@ fun RegisterScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            state = rememberTextFieldState(),
-            label = { Text("Vorname") },
-        )
-        Spacer(modifier = Modifier.height(20.dp))
-
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            state = rememberTextFieldState(),
-            label = { Text("Nachname") },
-        )
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            state = rememberTextFieldState(),
-            label = { Text("Benutzername") },
-        )
-
-        Spacer(modifier = Modifier.height(20.dp))
-
         EmailTextField()
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        PasswordTextField("Passwort")
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        PasswordTextField("Passwort wiederholen")
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Button(  modifier = Modifier.fillMaxWidth(),
+        Button(
+            modifier = Modifier.fillMaxWidth(),
             onClick = { }) {
-            Text("Registrieren")
+            Text("Passwort zurücksetzten")
         }
+
     }
 }
 
+@Composable
+fun EmailTextField() {
+    var email by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = email,
+        onValueChange = { email = it },
+        label = { Text("Email") },
+        modifier = Modifier.fillMaxWidth()
+    )
+
+}
+
+
 @Preview(showSystemUi = true)
 @Composable
-fun RegisterScreenPreview() {
-    RegisterScreen()
+fun PWForgottenScreenPreview() {
+    PWForgottenScreen()
 }
