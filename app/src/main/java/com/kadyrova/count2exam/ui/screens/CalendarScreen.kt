0cea3e5d6ff28607471a1fecf62dc358775b98e6
Alia Alamer <aliaalamer6582@gmail.com>
2026-06-13 20:58:03 +0200

    add header title (september) to calendarwidget

---
+++ app/src/main/java/com/kadyrova/count2exam/ui/screens/CalendarScreen.kt
@@ -0,0 +1,48 @@
+import android.R
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kadyrova.count2exam.ui.screens.FirstScreen
+import java.nio.file.WatchEvent
+
+@Composable
+fun CalendarWdiget(){
+    Column(
+        modifier = Modifier
+            .width(280.dp)
+            .background(Color .White, shape = RoundedCornerShape(16.dp))
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "September",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color(0xFFD81B60)
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CalendarWdigetPreview() {
+    CalendarWdiget()
+}
