f07921181bbaeda168b6235a72614e4604834a1e
Alia Alamer <aliaalamer6582@gmail.com>
2026-06-15 21:51:45 +0200

    add Fach input field to edit exam screen

--- app/src/main/java/com/kadyrova/count2exam/ui/screens/EditExamScreen.kt
+++ app/src/main/java/com/kadyrova/count2exam/ui/screens/EditExamScreen.kt
@@ -1,8 +1,14 @@
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
@@ -11,10 +17,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 
 @Composable
 fun EditExamScreen(){
+
+    var fach by remember { mutableStateOf("Robotics") }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -25,14 +37,37 @@ fun EditExamScreen(){
             fontSize = 32.sp,
             fontWeight = FontWeight.Bold
         )
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(45.dp))
 
         Text(
             text = "Prüfung bearbeiten",
             fontSize = 16.sp,
             fontStyle = FontStyle.Italic
         )
-        Spacer(modifier = Modifier.height(32.dp))
+        Spacer(modifier = Modifier.height(100.dp))
+
+        Text(
+            text = "Fach",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+
+        OutlinedTextField(
+            value = fach,
+            onValueChange = {fach = it},
+            placeholder = {Text("Robotics")},
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Bearbeiten"
+
+                )
+            },
+            shape = RoundedCornerShape(12.dp),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
 
