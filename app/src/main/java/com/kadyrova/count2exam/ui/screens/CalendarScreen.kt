8852533e5c8737f724494ead0cb2b5ad2edf5dba
Alia Alamer <aliaalamer6582@gmail.com>
2026-06-15 10:13:37 +0200

    add day grid with highlight to CalendarWidget

--- app/src/main/java/com/kadyrova/count2exam/ui/screens/CalendarScreen.kt
+++ app/src/main/java/com/kadyrova/count2exam/ui/screens/CalendarScreen.kt
@@ -1,14 +1,19 @@
 import android.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -17,6 +22,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -60,10 +66,41 @@ fun CalendarWdiget(){
         }
         Spacer(modifier = Modifier.height(8.dp))
 
+
+
+        val heighlightedDay = 22
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(7),
+            modifier = Modifier.height(180.dp)
+        ) {
+            items(30){ index ->
+                val day = index + 1
+                val isHighlighted = day == heighlightedDay
+
+                Box(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .aspectRatio(1f)
+                        .clip(CircleShape)
+                        .background(
+                            if(isHighlighted) Color(0xFFF8BBD0) else Color.Transparent
+                        ),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = day.toString(),
+                        fontSize = 12.sp,
+                        color = Color.Black
+                    )
+                }
+            }
+        }
+
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 fun CalendarWdigetPreview() {
     CalendarWdiget()
