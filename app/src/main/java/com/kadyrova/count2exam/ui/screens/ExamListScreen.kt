beaca42d0f907f630933bc81f70995919039db7f
Linda Kadyrova <linda.kadyrova@edu.fh-joanneum.at>
2026-06-17 21:56:45 +0200

    feat: Add ExamListScreen.kt

---
+++ app/src/main/java/com/kadyrova/count2exam/ui/screens/ExamListScreen.kt
@@ -0,0 +1,76 @@
+package com.kadyrova.count2exam.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kadyrova.count2exam.ui.components.AppHeader
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+
+@Composable
+fun ExamListScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        AppHeader()
+
+        Text(
+            text = "Meine Prüfungen",
+            fontSize = 20.sp,
+            modifier = Modifier.padding(34.dp)
+        )
+        ExamCard()
+    }
+}
+
+@Composable
+fun ExamCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 34.dp, vertical = 8.dp),
+        shape = RoundedCornerShape(16.dp),
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text("Robotics - 11.02.2026")
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            IconButton(onClick = { }) {
+                Icon(Icons.Default.Edit, contentDescription = "Bearbeiten")
+            }
+
+            IconButton(onClick = { }) {
+                Icon(Icons.Default.Delete, contentDescription = "Löschen")
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ExamListScreenPreview() {
+    ExamListScreen()
+}
