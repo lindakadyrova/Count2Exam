fddb4447de0a223405c760db78ca69e55cda5c2e
Linda Kadyrova <linda.kadyrova@edu.fh-joanneum.at>
2026-06-20 18:13:50 +0200

    feat: add exam reminder notifications

--- app/src/main/java/com/kadyrova/count2exam/utils/ExamNotificationReceiver.kt
+++ app/src/main/java/com/kadyrova/count2exam/utils/ExamNotificationReceiver.kt
@@ -1,17 +1,32 @@
 package com.kadyrova.count2exam.utils
 
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
+import androidx.core.app.NotificationCompat
 
 class ExamNotificationReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        Toast.makeText(
+
+        val notification = NotificationCompat.Builder(
             context,
-            "Prüfungserinnerung ausgelöst",
-            Toast.LENGTH_LONG
-        ).show()
+            NotificationHelper.CHANNEL_ID
+        )
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setContentTitle("Prüfungserinnerung")
+            .setContentText("Eine Prüfung steht bald an!")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE)
+                    as NotificationManager
+
+        notificationManager.notify(
+            1,
+            notification
+        )
     }
 }
